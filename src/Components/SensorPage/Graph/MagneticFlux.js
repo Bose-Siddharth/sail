01c83005484c2776892e00ad34e7b64192513567
Sayan Halder <sayan.halder@iemlabs.com>
2023-07-31 20:16:50 +0530

    new graph | partialy done

--- src/Components/SensorPage/Graph/MagneticFlux.js
+++ src/Components/SensorPage/Graph/MagneticFlux.js
@@ -1,104 +1,45 @@
-//npm install react-apexcharts apexcharts
+import React from "react";
+import StreamingPlugin from "chartjs-plugin-streaming";
+import { Line } from "react-chartjs-2";
+import "chartjs-adapter-luxon";
+import { Chart as ChartJS} from "chart.js";
+import "chart.js/auto"
 
-import React, { useState, useEffect, useRef } from 'react';
-import ReactApexChart from 'react-apexcharts';
+ChartJS.register(StreamingPlugin);
 
-const XAXISRANGE = 1800000;
-
-const getRandomData = (lastDate) => {
-  const randomDataPoint = Math.floor(Math.random() * 90) + 10;
-  return {
-    x: lastDate + XAXISRANGE,
-    y: randomDataPoint,
-  };
-};
-
-function MagneticFlux({ sensorData }) {
-  const [series, setSeries] = useState([
-    {
-      data: []
-    }
-  ]);
-
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    let lastDate = Date.now() - XAXISRANGE;
-    const interval = setInterval(() => {
-      lastDate += XAXISRANGE;
-      const newDataPoint = getRandomData(lastDate);
-      setSeries((prevSeries) => [
-        {
-          data: [...prevSeries[0].data, newDataPoint]
-        }
-      ]);
-      if (chartRef.current) {
-        setSeries((prevSeries) => [
-          {
-            data: [...prevSeries[0].data, newDataPoint]
-          }
-        ]);
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
+export default function MagneticFlux() {
   return (
-    <div id="chart">
-      <ReactApexChart
-        options={{
-          chart: {
-            id: 'realtime',
-            height: 350,
-            type: 'line',
-            animations: {
-              enabled: true,
-              easing: 'linear',
-              dynamicAnimation: {
-                speed: 1000
-              }
-            },
-            toolbar: {
-              show: false
-            },
-            zoom: {
-              enabled: false
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'smooth'
-          },
-          title: {
-            text: 'Dynamic Updating Chart',
-            align: 'left'
-          },
-          markers: {
-            size: 0
-          },
-          xaxis: {
-            type: 'datetime',
-            range: XAXISRANGE
-          },
-          yaxis: {
-            max: 100
-          },
-          legend: {
-            show: false
+    <Line
+      data={{
+        datasets: [
+          {
+            label: "Dataset 1",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            borderColor: "rgb(255, 99, 132)",
+            borderDash: [8, 4],
+            fill: true,
+            data: [],
           }
-        }}
-        series={series}
-        type="line"
-        height={350}
-        ref={chartRef}
-      />
-    </div>
+        ],
+      }}
+      options={{
+        scales: {
+          x: {
+            type: "realtime",
+            realtime: {
+              delay: 2000,
+              onRefresh: (chart) => {
+                chart.data.datasets.forEach((dataset) => {
+                  dataset.data.push({
+                    x: Date.now(),
+                    y: Math.random(),
+                  });
+                });
+              },
+            },
+          },
+        },
+      }}
+    />
   );
 }
-
-export default MagneticFlux;
